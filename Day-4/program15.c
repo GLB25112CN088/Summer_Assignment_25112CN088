fd14cc879c53ca34961036734316e66ab70c0cf4
Arun Kumar <subhikushwaha2004@gmail.com>
2026-05-31 21:03:12 +0530

    Add Day-4 program15 armstrong or not

---
+++ Day-4/program15.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int n,s=0,d,temp;
+    printf("enter the number:");
+    scanf("%d",&n);
+    temp=n;
+    while(n>0)
+    {
+        d= n%10;
+        s= s+d*d*d;
+        n= n/10;
+    }
+    if(s==temp)
+    {
+        printf("number is armstrong");
+    }
+    else 
+    {
+        printf("number is not armstrong");
+    }
+    return 0;
+}
