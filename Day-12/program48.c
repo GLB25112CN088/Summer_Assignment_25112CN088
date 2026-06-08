df041e5138dcbf9a36663172f950bbe52fcaf4d2
Arun Kumar <subhikushwaha2004@gmail.com>
2026-06-08 08:51:10 +0530

    Add Day-12 program48 functions for perfect number

---
+++ Day-12/program48.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int perfect(int n)
+{
+    int i,sum=0;
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        sum+=i;
+    }
+    return (sum==n);
+}
+int main()
+{
+    int n;
+    printf("enter number:");
+    scanf("%d",&n);
+    
+    if(perfect(n))
+    printf("perfect number");
+    else
+    printf("not perfect number");
+    return 0;
+}
