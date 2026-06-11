722b97e28fd5ec8b484fb1a7cdce12a611b419eb
Arun Kumar <subhikushwaha2004@gmail.com>
2026-06-11 15:23:31 +0530

    Add Day-15 program58 rotate array left

---
+++ Day-15/program58.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],n,i,temp;
+
+    printf("enter size of array:");
+    scanf("%d",&n);
+
+    printf("enter elements:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    temp=a[0];
+    for(i=0;i<n-1;i++)
+    {
+        a[i] = a[i+1];
+    }
+    a[n-1]=temp;
+
+    printf("array after left rotation:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+    return 0;
+
+}
