c52339aafb374f12d4bb0707cbb971d784f7d605
Arun Kumar <subhikushwaha2004@gmail.com>
2026-06-17 09:19:41 +0530

    Add Day-21 program82 reverse a string

---
+++ Day-21/program82.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    char str[100],temp;
+    int i,len=0;
+
+    printf("enter a string:");
+    scanf("%s",str);
+    while(str[len]!='\0')
+    {
+        len++;
+    }
+    for(i=0;i<len/2;i++){
+        temp=str[i];
+        str[i]=str[len-1-i];
+        str[len-1-i]=temp;
+    }
+    printf("reversed string = %s",str);
+
+    return 0;
+}
